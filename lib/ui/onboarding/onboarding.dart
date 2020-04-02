6e69fb29081f24796c0647e84989dc670dd6e7ab
Helder Pinhal <helder@notifica.re>
2020-04-02 18:18:47 +0100

    Add view pager to onboarding

--- lib/ui/onboarding/onboarding.dart
+++ lib/ui/onboarding/onboarding.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:notificare_push_lib/notificare_models.dart';
+import 'package:notificare_push_lib/notificare_push_lib.dart';
 
 class Onboarding extends StatefulWidget {
   @override
@@ -6,15 +8,49 @@ class Onboarding extends StatefulWidget {
 }
 
 class _OnboardingState extends State<Onboarding> {
+  final _notificare = NotificarePushLib();
+  PageController _pageController;
+  List<NotificareAsset> _onboardingAssets = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _pageController = PageController();
+
+    _fetchOnboarding();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _pageController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Onboarding'),
       ),
-      body: Center(
-        child: Text('Onboarding'),
+      body: PageView(
+        controller: _pageController,
+        children: _onboardingAssets.map((asset) {
+          return Center(
+            child: Text(asset.assetTitle),
+          );
+        }).toList(),
       ),
     );
+  }
+
+  _fetchOnboarding() async {
+    try {
+      final assets = await _notificare.fetchAssets("ONBOARDING");
+      setState(() {
+        _onboardingAssets = assets;
+      });
+    } catch (err) {
+      debugPrint('Failed to load the onboarding assets: $err');
+    }
   }
 }
