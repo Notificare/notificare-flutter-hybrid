0ca5805a1965315d6cb25bafc0b8973816ef9607
Helder Pinhal <helder@notifica.re>
2020-04-03 10:37:03 +0100

    Add onboarding UI

--- lib/ui/onboarding/onboarding.dart
+++ lib/ui/onboarding/onboarding.dart
@@ -1,3 +1,4 @@
+import 'package:demo_flutter/theme/notificare_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:notificare_push_lib/notificare_models.dart';
 import 'package:notificare_push_lib/notificare_push_lib.dart';
@@ -38,10 +39,51 @@ class _OnboardingState extends State<Onboarding> {
       body: PageView(
         controller: _pageController,
         children: _onboardingAssets.map((asset) {
-          return Container(
-            color: Colors.red,
-            child: Image.network(asset.assetUrl,
-                alignment: AlignmentDirectional.topCenter),
+          return Stack(
+            alignment: AlignmentDirectional.topCenter,
+            fit: StackFit.expand,
+            children: <Widget>[
+              Image.network(asset.assetUrl,
+                  alignment: AlignmentDirectional.topCenter),
+              Container(
+                padding: EdgeInsets.fromLTRB(20, 0, 20, 100),
+                child: Column(
+                  mainAxisSize: MainAxisSize.max,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Expanded(child: Container()),
+                    Container(
+                      padding: EdgeInsets.only(bottom: 30),
+                      child: Text(
+                        asset.assetTitle,
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          color: NotificareColors.gray,
+                          fontSize: 20,
+                          fontFamily: 'Lato',
+                        ),
+                      ),
+                    ),
+                    ConstrainedBox(
+                      constraints:
+                          const BoxConstraints(minWidth: double.infinity),
+                      child: RaisedButton(
+                        color: NotificareColors.outerSpace,
+                        textColor: Colors.white,
+                        padding: EdgeInsets.all(15),
+                        child: Text(
+                          asset.assetButton.label.toUpperCase(),
+                          style: TextStyle(
+                            fontFamily: 'Lato',
+                          ),
+                        ),
+                        onPressed: () => _onButtonPressed(asset),
+                      ),
+                    )
+                  ],
+                ),
+              )
+            ],
           );
         }).toList(),
       ),
@@ -59,4 +101,6 @@ class _OnboardingState extends State<Onboarding> {
       debugPrint('Failed to load the onboarding assets: $err');
     }
   }
+
+  _onButtonPressed(NotificareAsset asset) {}
 }
