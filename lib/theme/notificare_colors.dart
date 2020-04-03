477cc25a1f793a47a8e40cf08eeaac6d75aac355
Helder Pinhal <helder@notifica.re>
2020-04-03 10:36:43 +0100

    Add fonts & colours

---
+++ lib/theme/notificare_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class NotificareColors {
+  NotificareColors._();
+
+  static const gray = Color.fromARGB(255, 136, 136, 136);
+  static const outerSpace = Color.fromARGB(255, 35, 44, 42);
+}
