52c03b631e6b49bfd609a0af332d173a7e3bbbb9
Helder Pinhal <helder@notifica.re>
2020-04-16 16:33:01 +0100

    Add member card page

---
+++ lib/ui/member_card.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+class MemberCard extends StatelessWidget {
+  final String serial;
+
+  MemberCard({this.serial});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Member Card'),
+      ),
+      body: WebView(
+        javascriptMode: JavascriptMode.unrestricted,
+        onWebViewCreated: (controller) {
+          controller.loadUrl(
+            'https://push.notifica.re/pass/web/$serial?showWebVersion=1',
+          );
+        },
+      ),
+    );
+  }
+}
