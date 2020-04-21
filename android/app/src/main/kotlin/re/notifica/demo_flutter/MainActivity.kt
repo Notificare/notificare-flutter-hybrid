0ae1225cf7e0e6debd5991c46a554850abafae88
Helder Pinhal <helder@notifica.re>
2020-04-21 12:07:28 +0100

    Clean unused imports

--- android/app/src/main/kotlin/re/notifica/demo_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/re/notifica/demo_flutter/MainActivity.kt
@@ -1,9 +1,6 @@
 package re.notifica.demo_flutter
 
-import androidx.annotation.NonNull;
 import io.flutter.embedding.android.FlutterActivity
-import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
     // You can keep this empty class or remove it. Plugins on the new embedding
